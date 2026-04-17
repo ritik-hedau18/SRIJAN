5e443a63141cf6a0e0b6a6d7cb30a4deb0993451
ritik-hedau18 <ritikhedau253@gmail.com>
2026-04-17 12:25:22 +0530

    feat: add UserRepository with findByEmail query

--- src/main/java/com/srijan/srijan_backend/user/UserRepository.java
+++ src/main/java/com/srijan/srijan_backend/user/UserRepository.java
@@ -1,4 +1,8 @@
 package com.srijan.srijan_backend.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
